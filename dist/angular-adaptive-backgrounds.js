ad30a3a517cf852635bb09f418b5a8a1f623a2f4
brandly <brandly13@gmail.com>
2014-11-02 17:51:03 -0500

    reverse API so adaptive-background is placed on element receiving the background

--- dist/angular-adaptive-backgrounds.js
+++ dist/angular-adaptive-backgrounds.js
@@ -7,7 +7,7 @@
     !function(n){"use strict";var t=function(){return document.createElement("canvas").getContext("2d")},e=function(n,e){var a=new Image,o=n.src||n;"data:"!==o.substring(0,5)&&(a.crossOrigin="Anonymous"),a.onload=function(){var n=t("2d");n.drawImage(a,0,0);var o=n.getImageData(0,0,a.width,a.height);e&&e(o.data)},a.src=o},a=function(n){return["rgb(",n,")"].join("")},o=function(n){return n.map(function(n){return a(n.name)})},r=5,i=10,c={};c.colors=function(n,t){t=t||{};var c=t.exclude||[],u=t.paletteSize||i;e(n,function(e){for(var i=n.width*n.height||e.length,m={},s="",d=[],f={dominant:{name:"",count:0},palette:Array.apply(null,new Array(u)).map(Boolean).map(function(){return{name:"0,0,0",count:0}})},l=0;i>l;){if(d[0]=e[l],d[1]=e[l+1],d[2]=e[l+2],s=d.join(","),m[s]=s in m?m[s]+1:1,-1===c.indexOf(a(s))){var g=m[s];g>f.dominant.count?(f.dominant.name=s,f.dominant.count=g):f.palette.some(function(n){return g>n.count?(n.name=s,n.count=g,!0):void 0})}l+=4*r}if(t.success){var p=o(f.palette);t.success({dominant:a(f.dominant.name),secondary:p[0],palette:p})}})},n.RGBaster=n.RGBaster||c}(window);;
     var options;
     options = {
-      parentClass: null,
+      imageClass: null,
       exclude: ['rgb(0,0,0)', 'rgba(255,255,255)'],
       normalizeTextColor: false,
       normalizedTextColors: {
@@ -36,48 +36,42 @@
     return {
       restrict: 'A',
       link: function(scope, element, attrs) {
-        var adaptBackground, getParent, handleImg, rawElement, setColors, useCSSBackground;
+        var adaptBackground, childElement, findImage, handleImg, rawChildElement, rawElement, useCSSBackground;
         rawElement = element[0];
-        useCSSBackground = function() {
-          return attrs.abCssBackground != null;
+        useCSSBackground = function(el) {
+          return el.tagName !== 'IMG';
         };
-        getParent = function() {
-          var parent, parentSelector;
-          parentSelector = attrs.abParentClass || options.parentClass;
-          if (parentSelector) {
-            parent = element.parent();
-            while (parent[0] !== document) {
-              if (parent.hasClass(parentSelector)) {
-                return parent;
-              }
-              parent = parent.parent();
+        findImage = function() {
+          var elementWithClass, imageClass;
+          imageClass = attrs.abImageClass || options.imageClass;
+          if (imageClass != null) {
+            elementWithClass = rawElement.querySelector('.' + imageClass);
+            if (elementWithClass != null) {
+              return angular.element(elementWithClass);
             }
           }
-          return element.parent();
-        };
-        setColors = function(dominant, palette) {
-          var parent;
-          parent = getParent();
-          return parent.css('backgroundColor', dominant);
+          return angular.element(element.find('img')[0]);
         };
         adaptBackground = function(image) {
           return RGBaster.colors(image, {
             paletteSize: 20,
             exclude: options.exclude,
             success: function(colors) {
-              return setColors(colors.dominant, colors.palette);
+              return element.css('backgroundColor', colors.dominant);
             }
           });
         };
-        if (useCSSBackground()) {
-          return adaptBackground(getCSSBackground(rawElement));
+        childElement = findImage();
+        rawChildElement = childElement[0];
+        if (useCSSBackground(rawChildElement)) {
+          return adaptBackground(getCSSBackground(rawChildElement));
         } else {
           handleImg = function() {
-            return adaptBackground(rawElement);
+            return adaptBackground(rawChildElement);
           };
-          element.on('load', handleImg);
+          childElement.on('load', handleImg);
           scope.$on('$destroy', function() {
-            return element.off('load', handleImg);
+            return childElement.off('load', handleImg);
           });
           return handleImg();
         }
